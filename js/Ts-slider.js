8c28e97f2e6cd7914f9edc3ac619947f9fa54eaa
tsunamiShi <tsunamiShi@gmail.com>
2018-07-18 23:13:52 +0800

    第一次写插件，响应式布局已经基本掌握

---
+++ js/Ts-slider.js
@@ -0,0 +1,244 @@
+(function($){
+
+	$.fn.tsSlide = function(settings){
+
+		var defaults = {
+			width      : 960,
+			height     : 400,
+			position   : 1,
+			interval   : 5000,
+			duration   : 500,
+			animation  : true,
+			paginator  : true,
+			navigator  : true,
+			progress   : true,
+			skin       : 'tsSlide',
+			percentage : 0
+		};
+
+		var options = $.extend({}, defaults, settings);
+
+		this.each(function(){
+
+			var slider = $(this),
+				sliderLi = slider.find('li');
+			// sliderLi.length == 0 && console.error('error: empty slider!');
+
+			var wrapper = slider
+					.wrap('<div class="' + options.skin + '"/>')
+					.parent()
+					.css({
+						'width': options.width,
+						'background-size': '100% 100%'
+					}),
+				startPosition = options.position;
+
+			if(options.position == 0 || options.position > sliderLi.length){
+				// console.error('error: start position value must be between 1 and ' + sliderLi.length + '!');
+				startPosition = 1;
+			}
+			
+			slider
+				.on('mouseenter', function(){
+					$(this).addClass('hover');
+					hoverControl();
+				})
+				.on('mouseleave', function(){
+					$(this).removeClass('hover');
+					hoverControl();
+				})
+				.css('height', options.height);
+			
+			sliderLi
+				.css('width', options.width)
+				.filter(':nth-child(' + startPosition + ')')
+				.addClass('current');
+
+			//Controls (navigation, pagination and progress bar)
+			var position, paginator, paginatorLi, progress, progressWidth, progressElapsed, interact = false, sliderHeight;
+
+			if((options.navigator || options.paginator) && sliderLi.length > 1){
+				
+
+				if(options.paginator){
+					paginator = wrapper
+						.append('<ul class="paginator"/>')
+						.find('.paginator')
+						.hide();
+
+					sliderLi.each(function(){
+						paginator.append('<li>&nbsp;</li>');
+					});
+
+					paginatorLi = paginator
+						.find('li')
+						.on('click', function(){
+							if(interact){
+								position = $(this).index();
+								// console.log('position='+position+'   index='+index);
+								if((index - 1) != position){
+									sliderLi
+										.removeClass('current')
+										.filter(':nth-child(' + ++position + ')')
+										.addClass('current');
+									play();
+								}
+							}
+						})
+				}
+				if(options.navigator){
+					wrapper
+						.append('<div class="navigator prev"><span /></div><div class="navigator next"><span /></div>')
+						.find('.navigator')
+						.hide()
+						.on('click', function(){
+							var btn = $(this);
+							btn.hasClass('next') && interact && next();
+							btn.hasClass('prev') && interact && prev();
+						});
+				}
+			}
+
+			progress = wrapper
+				.prepend('<div class="progress"/>')
+				.find('.progress').width(0);
+
+			!options.progress && progress.height(0);
+
+			//Functions (init, play, next, prev, pause, resume)
+			var timeLeft = options.interval, current, index, paused;
+
+			function init(){				
+				progressResize();
+				sliderLi.length > 1 ? play() : sliderLi.fadeIn(options.duration);
+			}
+
+			function play(){
+				progressReset();
+				interact = false;
+				current = sliderLi
+					.filter('.current')
+					.siblings()
+					.fadeOut(options.animation && options.duration || 0)
+					.end()
+					.fadeIn(options.animation && options.duration || 0, function(){
+						interval();
+					});
+
+				$('.navigator')
+					.add('.paginator')
+					.fadeIn(options.animation && options.duration || 0);
+
+				index = sliderLi.index(current) + 1;
+				
+				if(options.paginator){
+					paginatorLi
+						.removeClass('current')
+						.filter(':nth-child(' + index + ')')
+						.addClass('current');
+				}
+			}
+
+			function next(){
+				sliderLi.removeClass('current');
+				if(++index <= sliderLi.length){
+					current
+						.next()
+						.addClass('current')
+				} else {
+					sliderLi
+						.filter(':first-child')
+						.addClass('current');
+				}
+				play();
+			}
+
+			function prev(){
+				sliderLi.removeClass('current');
+				if(--index >= 1){
+					current
+						.prev()
+						.addClass('current')
+				} else {
+					sliderLi
+						.filter(':last-child')
+						.addClass('current');
+				}
+				play();
+			}
+
+			function pause(){
+				paused = true;
+				progressElapsed = progress
+					.stop()
+					.width();
+				timeLeft = (progressWidth - progressElapsed) * (options.interval / progressWidth);
+			}
+
+			function interval(){
+				paused = false;
+				interact = true;
+				progress
+					.stop()
+					.show()
+					.animate({
+						width: '+=' + (progressWidth - progressElapsed)
+					}, timeLeft, 'linear', function(){
+						progressReset();
+						next();
+					});
+				hoverControl();
+			}
+
+			function progressReset(){
+				progress.stop().width(0);				
+				progressElapsed = 0;
+				timeLeft = options.interval;
+			}
+
+			function progressResize(){
+				$(window)
+					.resize(function(){
+						console.log('触发');
+						console.log(window.innerWidth)
+						progressWidth = slider.width();
+						
+						if(window.innerWidth <= 782 ){
+							options.percentage = 64 / 77;
+						}else{
+							options.percentage = 144 / 55;
+						}
+						//setHeight
+						setHeight(options.percentage);
+						pause(); interval();
+					}).resize();
+			}
+
+			function setHeight(percentage){
+				if(percentage !== 0){
+					sliderHeight = slider.width() / percentage;
+					// console.log(slider);
+					slider.height(sliderHeight);
+				}
+			}
+			function hoverControl(){
+				if(interact){
+					// console.log(slider);
+					if(slider.hasClass('hover')){
+						pause();
+                        // console.log('pause');
+
+					} else if(paused){
+						interval();
+                        // console.log('pause-over');
+
+					}
+				}
+			}
+
+			
+			init();
+
+		});
+	}
+})(jQuery);
---
+++ js/my-slide.js
@@ -0,0 +1,160 @@
+(function ($) {
+
+    $.fn.mySlide = function (settings) {
+        var defaults = {
+            num: [5],
+            widthSwitch: [],
+            interval: 300
+        }
+        // console.log(Math.floor(-0.21))
+
+        var options = $.extend({}, defaults, settings);
+
+        //简单判断一下，完整的是还需要数组内数字从大到小排列。
+        // 两种办法，报错，让用户修改参数，或者我给他排好。我这边默认它从大到小写了。
+        // (options.num.length - 1) === options.widthSwitch.length ? true : console.error("你输入的初始化数据有问题")
+
+        this.each(function () {
+            var $slideWrap = $('.my-slide-wrap'),
+                $slide = $('.my-slide'),
+                $slideUl = $('.slide-ul'),
+                $slideWidth = $slide.width(),   //轮播图展示出来的宽度
+                $slideLi = $slide.find('li'),   //总的li
+                $slideNum = $slideLi.length,    //li总个数
+                $slideLiWidth,  //li的宽度
+                $windowWidth,   //当前窗口的宽度
+                $liShowNum,     //展示的li的个数         
+                lock = true,
+                index = 1;
+
+            function getWindowWidth() {
+                return $(window).width();
+            }
+
+            var $prev = $slideWrap.find('.prev'),
+                $next = $slideWrap.find('.next');
+
+
+            // 点击 -> 检查状态(跳跃index) -> index增减
+            $prev.on('click', prev)
+            function prev() {
+                // var left = -$slideLiWidth * ($slideNum / 2);
+
+                // if ($slideUl.position().left === 0) {
+                //     $slideUl.css({
+                //         'left': left
+                //     })
+
+                //     index = $slideNum / 2 + 1;
+                // }
+                // console.log(index)
+
+                if (index === 1) {
+                    // console.log('prev-side');
+                    index = $slideNum / 2 + 1;
+                    $slideUl.css({
+                        'left': -(index - 1) * $slideLiWidth
+                    })
+                }
+                move('prev');
+            }
+            $next.on('click', next)
+
+            function next() {
+                // var left = -$slideLiWidth * ($slideNum - $liShowNum);
+                // var positionLeft = $slideUl.position().left;
+                // if (positionLeft === left) {
+                //     left = -$slideLiWidth * ($slideNum / 2 - $liShowNum);
+                //     $slideUl.css({
+                //         'left': left
+                //     })
+                //     index = $slideNum / 2;
+                // }
+                // console.log(index)
+                if (index === ($slideNum - $liShowNum + 1)) {
+                    // console.log('next-side')
+                    index = $slideNum / 2 - $liShowNum + 1;
+                    $slideUl.css({
+                        'left': -(index - 1) * $slideLiWidth
+                    })
+                }
+                move('next');
+            }
+
+            function move(direction) {
+                if (lock) {
+                    interval(direction);
+                }
+            }
+
+            function interval(sign) {
+                lock = false;
+                if (sign == 'prev') {
+                    sign = '+';
+                    index--;
+                } else if (sign === 'next') {
+                    sign = '-';
+                    index++;
+                };
+                $('.prev').off('click');
+                $('.next').off('click');
+                $slideUl
+                    .stop()
+                    .show()
+                    .animate({
+                        left: sign + '=' + $slideLiWidth
+                    }, 300, 'linear', function () {
+                        lock = true;
+                        $('.prev').on('click', prev);
+                        $('.next').on('click', next);
+                    })
+            }
+
+            windowResize();
+
+            function windowResize() {
+                $(window)
+                    .resize(function () {
+
+                        // console.log(index);
+                        $windowWidth = getWindowWidth();
+                        var arr = options.widthSwitch,
+                            len = arr.length,
+                            cnt = 0;
+                        for (var i = 0; i < len; i++) {
+                            if ($windowWidth > arr[i]) {
+                                break;
+                            }
+                            cnt++;
+                        }
+                        $liShowNum = options.num[cnt];
+                        setWidth($liShowNum);
+                        setSlideHeight();
+                        $slideUl
+                            .stop()
+                            .css({
+                                'left': -(index - 1) * $slideLiWidth
+                            }, 100, 'linear', function () {
+                                // console.log($slideUl.position().left)
+                                // console.log('over');
+                            })
+                    }).resize();
+            }
+
+            function setWidth(num) {
+                $slideWidth = Math.floor($slide.width());
+                $slideLiWidth = Math.floor($slideWidth / num);
+                $slideLi.width($slideLiWidth);
+                $slideUl.width($slideLiWidth * $slideNum);
+            }
+
+            function setSlideHeight() {
+                var height = $slideLi.height();
+                $slide.height(height);
+                $('.slide-turn-btn').height(height);
+            }
+        })
+
+    }
+
+})(jQuery)
